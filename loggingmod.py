598e824b62333bff9a15f1286c6839fc9decce2c
Matthew Mercuri <mercurimatthew@gmail.com>
2019-12-18 12:25:45 -0500

    fixed logging df to hold date as index

--- loggingmod.py
+++ loggingmod.py
@@ -10,12 +10,10 @@ class Log:
         self.log_df = self.log_df.set_index('Trade Date')
 
     def log_trade(self, date, symbol, action, quantity, price, trans_pnl):
-        ''' Need to figure out how to get the index to be the date of trade
-        '''
         data = [date, symbol, action, quantity, price, trans_pnl]
         trade_df = pd.DataFrame([data], columns=LOGCOLS)
         trade_df = trade_df.set_index('Trade Date')
-        self.log_df = pd.concat([self.log_df, trade_df], ignore_index=True)
+        self.log_df = pd.concat([self.log_df, trade_df])
 
     def save_log_locally(self):
         pass
